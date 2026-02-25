757f15bf3fc9e54e8d676f7955efdb40e813920d
steelebender <clement.arhin212@gmail.com>
2026-02-25 11:26:01 -0500

    v1.01

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const joi = require("joi");
 const mysql = require("mysql2");
-
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -16,9 +14,13 @@ const db = mysql.createConnection({
 });
 
 mongoose
-  .connect("mongodb://localhost:27017/test", {})
+  .connect("mongodb://localhost:27017/refastsql", {})
   .then(() => console.log("MOngoDB is Connected!"))
   .catch((err) => console.error("Connection error:", err));
+
+const userLogSchema = new mongoose.Schema({}, { collection: "user_logs" });
+
+const UserLog = mongoose.model("UserLog", userLogSchema);
 
 const port = process.env.PORT || 5001;
 
@@ -34,6 +36,16 @@ app.get("/users", (req, res) => {
     if (err) return res.json({ message: "Error fetching users" });
     res.json(result);
   });
+});
+
+app.get("/user-logs", async (req, res) => {
+  try {
+    const user_logs = await UserLog.find(); // query the model
+    res.json(user_logs);
+  } catch (err) {
+    console.error(err); // logs the real error
+    res.status(500).json({ message: "Error fetching user logs" });
+  }
 });
 
 app.listen(port, () => {
